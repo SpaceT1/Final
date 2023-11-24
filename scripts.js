e08dcf40f5cb61f68da6d31332067df359ff556a
Alikhan <battttmanalikhan@mail.ru>
2023-11-25 01:52:59 +0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// scripts.js
+
+// Play a sound when the page loads
+document.addEventListener('DOMContentLoaded', function () {
+    var audio = new Audio('music.mp3');
+    audio.play();
+  });
+  // Example JS Object with Properties
+  var factoryInfo = {
+    name: 'Willy Wonka Factory',
+    location: 'Chocolate Street, Candyland',
+    capacity: 1000,
+    isMagical: true
+  };
+  
+  console.log(factoryInfo);
+  
